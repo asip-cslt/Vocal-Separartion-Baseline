80638dc382318a0de1bbfa2b5cd89160968fde39
youzhenghai <youzhenghai@bupt.edu.cn>
2025-06-27 21:11:13 +0800

    debug env init

--- Sepformer/separation/example.sh
+++ Sepformer/separation/example.sh
@@ -1,5 +1,5 @@
 
-torchrun --nproc_per_node=2 train.py hparams/sepformer-4mix.yaml
+# torchrun --nproc_per_node=2 train.py hparams/sepformer_4mix.yaml
 
 
-#torchrun --nproc_per_node=2 train.py hparams/sepformer-4mix.yaml --test_only
+torchrun --nproc_per_node=2 train.py hparams/sepformer_4mix.yaml --test_only
